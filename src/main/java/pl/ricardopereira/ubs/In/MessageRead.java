c2d68c160802591e8d796bfccbc1bbfa35b09557
EduardoRenani00 <eduardo.renani@hotmail.com>
2018-02-21 00:14:00 -0300

    input classes

---
+++ src/main/java/pl/ricardopereira/ubs/In/MessageRead.java
@@ -0,0 +1,5 @@
+package pl.ricardopereira.ubs.In;
+
+public class MessageRead {
+	
+}
---
+++ src/main/java/pl/ricardopereira/ubs/In/MessageWrite.java
@@ -0,0 +1,22 @@
+package pl.ricardopereira.ubs.In;
+
+public class MessageWrite {
+	private String destiny;
+	private String message;
+	
+	public String getDestiny() {
+		return destiny;
+	}
+	
+	public void setDestiny(String destiny) {
+		this.destiny = destiny;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
