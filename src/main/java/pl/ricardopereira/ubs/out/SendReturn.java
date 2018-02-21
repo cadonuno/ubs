f570817cbed10f20faf6edacb2ff1ff513ecd479
EduardoRenani00 <eduardo.renani@hotmail.com>
2018-02-21 00:22:37 -0300

    uncommited service and pojo

---
+++ src/main/java/pl/ricardopereira/ubs/out/SendReturn.java
@@ -0,0 +1,15 @@
+package pl.ricardopereira.ubs.out;
+
+public class SendReturn {
+	private boolean messageSent;
+
+	public boolean isMessageSent() {
+		return messageSent;
+	}
+
+	public void setMessageSent(boolean messageSent) {
+		this.messageSent = messageSent;
+	}
+	
+	
+}
